177749cd0b1118dd783a0dfb173f74402d24fbd1
Cauã Ananias <101273900+caua-ananias@users.noreply.github.com>
2024-02-09 20:38:27 -0400

    Add files via upload

---
+++ CertificationApplication/src/main/java/com/rocketseat/certification_nlw/modules/students/repositories/CertificationStudentRepository.java
@@ -0,0 +1,16 @@
+package com.rocketseat.certification_nlw.modules.students.repositories;
+
+import com.rocketseat.certification_nlw.modules.students.entities.CertificationStudentEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.UUID;
+import java.util.List;
+public interface CertificationStudentRepository extends JpaRepository<CertificationStudentEntity, UUID> {
+
+    @Query("SELECT c FROM certifications c INNER JOIN c.studentEntity std WHERE std.email = :email AND c.technology = :technology")
+    List<CertificationStudentEntity> findByStudentEmailAndTechnology(String email, String technology);
+
+    @Query("SELECT c from certifications c ORDER by c.grade DESC LIMIT 10")
+    List<CertificationStudentEntity> findTop10ByOrderGradeDesc();
+}
